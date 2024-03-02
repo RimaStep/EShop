6c3ec629bab15ed2fabee5cea3c198d340e95bd6
Rima Stepanyan <rima.h.stepanyan@gmail.com>
2024-03-03 01:52:12 +0400

    Initial commit

---
+++ src/main/java/org/example/myeshop/MyEShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.myeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/myeshop/MyEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
